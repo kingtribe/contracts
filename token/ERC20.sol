62e4fe20a6abd0c6bc99cd5daf8f72c7f606d148
Your Name <you@example.com>
2022-10-08 20:26:47 +0800

    f

--- token/ERC20.sol
+++ token/ERC20.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+import "./IERC20.sol";
+import "./IERC20Metadata.sol";
+import "./Context.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
